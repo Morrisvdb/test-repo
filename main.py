9be9dada862358308b35e94c5cbc06ca5244dd02
Morris van den Bergh <morrisvandenbergh@gmail.com>
2023-06-02 20:28:55 +0000

    No Slash Command

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 client = commands.Bot(command_prefix = "!", intents=discord.Intents.all())
 
-@client.slash_command(name="test", guild_ids=[977513866097479760, 1047234879743611034])
+@client.command()
 async def test(ctx, arg):
-	await ctx.respond(arg)
+	await ctx.send(arg)
 
 client.run(DISCORD_TOKEN)
