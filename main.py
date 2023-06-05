166a178aee561fcc3a81d84556bf50ff3961a51e
Morris van den Bergh <morrisvandenbergh@gmail.com>
2023-06-05 05:28:35 +0000

    Again

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 
 DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
-bot = discord.Bot()
+bot = commands.Bot()
 
 @bot.slash_commands(name="test", description="Test", guild_ids=[977513866097479760, 1047234879743611034])
 async def test(ctx, arg):
